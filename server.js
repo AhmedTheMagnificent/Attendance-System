8368ae1057d817867fff0f14390321818da10d34
AhmedTheMagnificent <ahmedmanooo14102005@gmail.com>
2024-12-06 01:02:51 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.use(express.json())
+
+app.get("/tshirt", (req, res) => {
+    res.status(200).send({
+        "tshirt": "red",
+        "size": "small"
+    })
+});
+
+app.post("/tshirt/:id", (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+    if (!logo) {
+        res.status(418).send({message : "We need a logo!!"})
+    }
+    res.send({
+        tshirt : `We have the log ${logo} with id: ${id}`
+    })
+})
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
