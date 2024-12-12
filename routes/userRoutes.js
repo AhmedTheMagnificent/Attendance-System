fd22a32451230d058e5440c3731ecd912c679a0f
AhmedTheMagnificent <ahmedmanooo14102005@gmail.com>
2024-12-12 23:32:56 +0500

    added i guess the last commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -19,8 +19,29 @@ router.post("/login", async (req, res) => {
         return res.status(401).send("Invalid Credentials.");
     const token = jwt.sign({id : user._id}, process.env.JWT_SECRET, {expiresIn : "1h"});
     res.send({token});
+});
+
+router.post("/attendance/mark", protect, async (req, res) => {
+    const today = new Date().toISOString().split('T')[0];
+    const alreadyMarked = req.user.attendance.find(a => a.Date().toISOString().split("T")[0] === today);
+    if(alreadyMarked){
+        return res.status(400).send("Attendance already marked for today.");
+    }
+    await req.user.save();
+    res.send("Attendance marked");
+});
+
+router.get("/attendance/view", protect, (req, res) => {
+    res.send(req.user.attendance);
+});
+
+router.post("/leave/request", protect, async (req, res) => {
+    const { reason } = req.body;
+    req.user.leaveRequest.push({reason});
+    await req.user.save();
+    res.send("Leave request sent");
 })
 
-router.post("/attendance/mark", protect async (req, res) => {
-    
-})
+router.post("/profile/update")
+
+module.exports = router;
